a9eaef80423786d5904d28228b09a03a9abf05b9
Shruan <qiushiyuan1994@qq.com>
2018-06-09 03:27:03 +0800

    楼层组件化 推荐商品组件化

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -54,31 +54,31 @@
         </swiper>
       </div>
     </div>
+    <Floor :floorData="floor1" :floorTitle="floorName.floor1"/>
+    <Floor :floorData="floor2" :floorTitle="floorName.floor2"/>
+    <Floor :floorData="floor3" :floorTitle="floorName.floor3"/>
 
-    <div class="recommend-area">
-      <div class="recommend-title">
-        楼层
-      </div>
-      <div class="recommend-body">
-        <!--floor one area-->
-        <div class="floor">
-          <div class="floor-anomaly">
-            <div class="floor-one"><img :src="floor1_0.image" width="100%" /></div>
-            <div>
-              <div class="floor-two"><img :src="floor1_1.image" width="100%" /></div>
-              <div><img :src="floor1_2.image" width="100%" /></div>
-            </div>
-          </div>
+    <!--Hot Area-->
+    <div class="hot-area">
+        <div class="hot-title">热卖商品</div>
+        <div class="hot-goods">
+          <!--这里需要一个list组件-->
+          <van-list>
+            <van-row gutter="20">
+              <van-col
+                span="12"
+                v-for="(item,index) in hotGoods"
+                :key="index">
+                <goods-info
+                  :goodsImage="item.image"
+                  :goodsName="item.name"
+                  :goodsPrice="item.price"
+                />
+              </van-col>
+            </van-row>
+        </van-list>
         </div>
-
-        <div class="floor-rule">
-          <div v-for="(item ,index) in floor1.slice(3)" :key="index">
-            <img :src="item.image" width="100%"/>
-          </div>
-        </div>
-      </div>
     </div>
-
   </div>
 </template>
 
@@ -87,11 +87,16 @@ import { mapState } from 'vuex'
 
 import 'swiper/dist/css/swiper.css'
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import Floor from './subPages/Floor'
+import GoodsInfo from './subPages/GoodsInfo'
+import url from '@/utils/serviceApi.config.js'
 
 export default {
   components: {
     swiper,
-    swiperSlide
+    swiperSlide,
+    Floor,
+    GoodsInfo
   },
   data () {
     return {
@@ -108,9 +113,10 @@ export default {
       adBanner: [],
       recommendGoods: [],
       floor1: [],
-      floor1_0: {},
-      floor1_1: {},
-      floor1_2: {}
+      floor2: [],
+      floor3: [],
+      floorName: {},
+      hotGoods: []
     }
   },
   computed: {
@@ -123,7 +129,7 @@ export default {
   },
   methods: {
     loadList () {
-      this.$http.get('/api/index').then(res => {
+      this.$http.get(url.getShoppingMallInfo).then(res => {
         console.log(res.data)
         res = res.data
         this.bannerPicArray = res.slides
@@ -131,9 +137,10 @@ export default {
         this.adBanner = res.advertesPicture
         this.recommendGoods = res.recommend
         this.floor1 = res.floor1 // 楼层1数据
-        this.floor1_0 = this.floor1[0]
-        this.floor1_1 = this.floor1[1]
-        this.floor1_2 = this.floor1[2]
+        this.floor2 = res.floor2 // 楼层2数据
+        this.floor3 = res.floor3
+        this.floorName = res.floorName
+        this.hotGoods = res.hotGoods
       }).catch(res => {})
     }
   }
@@ -169,7 +176,7 @@ export default {
   }
   .type-bar {
     background-color: #fff;
-    margin:0 .3rem .3rem .3rem;
+    margin: 0 .3rem .3rem .3rem;
     border-radius: .3rem;
     font-size:14px;
     display: flex;
@@ -204,38 +211,11 @@ export default {
     text-align: center;
   }
 
-  .floor-anomaly {
-     display: flex;
-     flex-direction:row;
-     background-color: #fff;
-     border-bottom:1px solid #ddd;
- }
-  .floor-anomaly div{
-    width:10rem;
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-  }
-  .floor-one {
-    border-right:1px solid #ddd;
-  }
- .floor-two {
-    border-bottom:1px solid #ddd;
-  }
-  .floor-rule{
-    display: flex;
-    flex-direction: row;
-    flex-wrap:wrap;
-    background-color: #fff;
-
-  }
-  .floor-rule div {
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    width:10rem;
-    border-bottom:1px solid #ddd;
-  }
-  .floor-rule div:nth-child(odd) {
-    border-right: 1px solid #ddd;
+  .hot-area{
+    text-align: center;
+    font-size:14px;
+    height: 1.8rem;
+    line-height:1.8rem;
   }
 
 </style>
