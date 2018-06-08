bba2cd0ad06a20457023883f95079359d6d0ed08
Shruan <qiushiyuan1994@qq.com>
2018-06-09 03:26:07 +0800

    请求模块化 接口抽离

---
+++ src/utils/serviceApi.config.js
@@ -0,0 +1,7 @@
+const BASEURL = 'https://easy-mock.com/mock/5af01dd24a095174a7d86d1e'
+const URL = {
+  getShoppingMallInfo: BASEURL + '/api/index',
+  getGoodsInfo: BASEURL + 'getGoodsInfo'
+}
+
+module.exports = URL
---
+++ src/utils/utils.js
@@ -0,0 +1,4 @@
+// 格式化 数据 保留两位小时
+export function toMoney (money = 0) {
+  return money.toFixed(2)
+}
