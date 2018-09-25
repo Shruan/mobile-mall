3edb8ddf0003df4b2e5f04201f1cebf5c2bf29ae
Shruan <qiushiyuan1994@qq.com>
2018-09-26 02:44:58 +0800

    注册界面功能完善

--- src/pages/register/index.vue
+++ src/pages/register/index.vue
@@ -1,44 +1,46 @@
 <template>
   <div>
-     <van-nav-bar
+    <van-nav-bar
       title="用户注册"
       left-text="返回"
       left-arrow
       @click-left="goBack"
-      />
-
+    />
       <div class="register-panel">
+        
       <van-field
-          v-model="form.username"
-          label="用户名"
-          icon="clear"
-          placeholder="请输入用户名"
-          required
-          @click-icon="form.username = ''"
+        required
+        v-model="form.username"
+        label="用户名"
+        icon="clear"
+        placeholder="请输入用户名"
+        @click-icon="form.username = ''"
       />
 
       <van-field
-          v-model="form.password"
-          type="password"
-          label="密码"
-          placeholder="请输入密码"
-          required
+        v-model="form.password"
+        type="password"
+        label="密码"
+        placeholder="请输入密码"
+        required
       />
       <div class="register-button">
-          <van-button
-            type="primary"
-            size="large"
-            @click.native="register">
-            马上注册
-          </van-button>
+        <van-button
+          type="primary"
+          size="large"
+          :loading="openLoading"
+          @click.native="register">
+          马上注册
+        </van-button>
       </div>
-     </div>
+    </div>
 
   </div>
 </template>
 
 <script>
 import url from '@/utils/serviceApi.config.js'
+import { Toast } from 'vant'
 
 import { mapState } from 'vuex'
 export default {
@@ -49,7 +51,8 @@ export default {
       form: {
         username: '',
         password: ''
-      }
+      },
+      openLoading: false // 是否开启用户的Loading
     }
   },
   computed: {
@@ -64,8 +67,22 @@ export default {
       this.$router.go(-1)
     },
     register () {
+      if (!this.form.username || !this.form.password) {
+        Toast.fail('账号密码不能为空')
+        return
+      }
+      this.openLoading = true
       this.$http.post(url.registerUser, this.form).then(res => {
-        console.log(res)
+        if (res.code == 200) {
+          Toast.success('注册成功')
+          this.$router.push({name: 'Login'})
+        } else {
+          Toast.fail('注册失败')
+          this.openLoading = false
+        }
+      }).catch(err => {
+        Toast.fail('注册失败')
+        this.openLoading = false
       })
     }
   }
