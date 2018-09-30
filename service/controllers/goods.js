23559ea76f4e653f64e977bd0094a941f2c63cc1
Shruan <qiushiyuan1994@qq.com>
2018-09-30 15:39:01 +0800

    商品类别接口

--- service/controllers/goods.js
+++ service/controllers/goods.js
@@ -1,14 +1,71 @@
 const mongoose = require('mongoose')
-const Goods = mongoose.model('Goods')
 
+// 获取商品详情
 const getDetailGoodsInfo = async (ctx) => {
   try {
+    const Goods = mongoose.model('Goods')
     let goodsId = ctx.request.query.goodsId
     let result = await Goods.findOne({ ID: goodsId }).exec()
     ctx.body = {
       success: true,
-      data: result,
-      message: '成功'
+      data: result
+    }
+  } catch (err) {
+    console.log(err)
+    ctx.body = {
+      success: false,
+      message: err
+    }
+  }
+}
+
+// 获取商品类别（大类）
+const getCategoryList = async (ctx) => {
+  try {
+    let goodsId = ctx.request.query.goodsId
+    const Category = mongoose.model('Category')
+    let result = await Category.findOne().exec()
+    ctx.body = {
+      success: true,
+      data: result
+    }
+  } catch (err) {
+    console.log(err)
+    ctx.body = {
+      success: false,
+      message: err
+    }
+  }
+}
+
+// 获取商品类别（小类）
+const getCategorySubList = async (ctx) => {
+  try {
+    let categoryId = ctx.request.query.categoryId
+    const CategorySub = mongoose.model('CategorySub')
+    let result = await CategorySub.findOne({ MALL_CATEGORY_ID: categoryId }).exec()
+    ctx.body = {
+      success: true,
+      data: result
+    }
+  } catch (err) {
+    console.log(err)
+    ctx.body = {
+      success: false,
+      message: err
+    }
+  }
+}
+
+// 商品类别获取商品列表
+const getGoodsListByCategorySubID = async (ctx) => {
+  try {
+    let categorySubId = ctx.request.query.categorySubId
+    const Goods = mongoose.model('Goods')
+    let result = await Goods.find({ SUB_ID: categorySubId })
+    ctx.body = {
+      success: true,
+      data: result
     }
   } catch (err) {
     console.log(err)
@@ -20,5 +77,8 @@ const getDetailGoodsInfo = async (ctx) => {
 }
 
 module.exports = {
-  'GET /getDetailGoodsInfo': getDetailGoodsInfo
+  'GET /getDetailGoodsInfo': getDetailGoodsInfo,
+  'GET /getCategoryList': getCategoryList,
+  'GET /getCategorySubList': getCategorySubList,
+  'GET /getGoodsListByCategorySubID': getGoodsListByCategorySubID
 }
